e041d05e709f280c57f45ee77c681d97a62e6637
Patrick Dunn <pmdunn78@gmail.com>
2020-01-08 15:38:26 -0500

    making AJAX call to weather API and dynamically changing forecast content

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+    var city = "";
     var citiesArray = [];
     getData();
 
@@ -10,21 +10,28 @@ $(document).ready(function() {
             event.preventDefault();
             setData();
             getData();
+            getWeather(city);
+            
         }
     });
 
     $(".btn-search").on("click", function() {
         setData();
         getData();
+        getWeather(city);
+        
     });
 
     function setData() {
-        var city = $("#city-search").val().trim();
+        city = $("#city-search").val().trim();
         citiesArray.push(city);
         for (var i = 0; i < citiesArray.length; i++) {
             localStorage.setItem(citiesArray[i], citiesArray[i]);
         }
         console.log(citiesArray);
+        console.log("city: " + city);
+
+        
     }
 
     function getData() {
@@ -42,17 +49,38 @@ $(document).ready(function() {
             darkBtn.attr("type", "button");
             //set button text()
             darkBtn.text(localStorage.getItem(citiesArray[i]));
+
+
             
             //add button functionality
+            var buttonVal = darkBtn.text();
+            darkBtn.on("click", function() {
+                getWeather(buttonVal);
+            });
 
             //set button as html of li
             li.html(darkBtn);
             
-            //append li to ul
+            //prepend li to ul
             $(".list-group").prepend(li);
         }
     }
 
-
-
+    var getWeather = function(place) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + place + "&units=imperial" + "&apikey=4d721e459b51eed9d4d8047d079984e6";
+        $.ajax({
+          url:queryURL,
+          method:"GET"
+        }).then(function(response) {
+          console.log(response);
+          //
+          $("#city-name").text(response.name + ", " + response.sys.country + " " + moment().format('l'));
+          $("#temp").text("Temperature: " + response.main.temp + " F");
+          $("#humidity").text("Humidity: " + response.main.humidity + "%");
+          $("#wind-speed").text("Wind-Speed: " + response.wind.speed + " mph");
+          $("#UV-index").text("UV Index: ");
+        })
+    };
+    
+    
 });
